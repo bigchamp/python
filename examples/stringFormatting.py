876b8114db323cdc7b68738b1050154adc8d50a8
Kairat Karzhauov <kairatkarzhauov@MacBook-Air-sulpak.local>
2019-10-08 14:55:12 +0600

    String Formatting

---
+++ examples/stringFormatting.py
@@ -0,0 +1,17 @@
+name = "John"
+print("Hello, %s!" % name)
+
+# %s - for strings
+name = "Martin"
+
+# %d - for integers
+age = 23
+
+# %f - for floating point numbers
+height = 180.5
+
+print("%s is %d years old.\nMy height is %f" % (name, age, height))
+
+# %.<number of digits>f - Floating point numbers with a fixed amount of digits to the right of the dot.
+
+# %x/%X - Integers in hex representation (lowercase/uppercase)
