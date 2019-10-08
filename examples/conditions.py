288d4979f3f90546ff7cd918bb792169e2628244
Kairat Karzhauov <kkarzhauov4@gmail.com>
2019-10-08 16:33:36 +0600

    Conditions

--- examples/conditions.py
+++ examples/conditions.py
@@ -1,8 +1,21 @@
-x = 0
-y = 0
-if x < y:
-    print(x, ' is low than', y)
-elif x == y:
-    print(x, 'equal to ', y)
-else:
-    print(x, 'is bigger than', y)
+x = 2
+
+print(x == 2)
+print(x == 3)
+print(x < 2)
+
+name = "Kairat"
+age = 23
+
+if name == "Kairat" and age == 23:
+    print("Your name is John, and you are also 23 years old.")
+if name == "Kairat" or name == "Rick":
+    print("Your name is either John or Rick.")
+
+
+# The "in" operator could be used to check if a specified object exists within an iterable object container, such as a list
+name = "John"
+listArr = ["John", "Rick"]
+
+if name in listArr:
+    print("Your name is either John or Rick.")
