b1b41fffbad071b1c98649f790ca4ea241f7d02b
Kairat Karzhauov <kkarzhauov4@gmail.com>
2019-10-08 16:23:56 +0600

    Basic string operations

--- examples/basicStringOperations.py
+++ examples/basicStringOperations.py
@@ -1,5 +1,17 @@
 astring = "Hello world"
 astring2 = "Hello world"
 astring3 = "Hello world"
-
 print(len(astring))
+
+astring = "Hello world!"
+print(astring.index("w"))
+print(astring.index("d"))
+print(astring[3 : 7])
+print(astring[3 : 7  :2])
+print(astring.upper())
+print(astring.lower())
+
+print(astring.startswith("He"))
+print(astring.endswith("d"))
+
+print(astring.split(" "))
